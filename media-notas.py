4cecf1bca67d083e9b0656e36bfcdc874ea3b50c
Rafael Martinez <30578302+rmartins87@users.noreply.github.com>
2017-11-07 17:32:13 -0200

    Calculates Media code

---
+++ media-notas.py
@@ -0,0 +1,9 @@
+notas=["n1","n2","n3","n4"]
+soma=0
+cont=0
+for nota in notas:
+    n1=float(input('entre com a nota: '))
+    soma=n1+soma
+    cont=cont+1
+print ('a media eh: '), soma/cont
+    
